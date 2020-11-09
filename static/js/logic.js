813b077c5638c31427211d6564b843eb9ca238dc
TiaraSGreen <65461540+TiaraSGreen@users.noreply.github.com>
2020-11-09 17:49:01 -0600

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -18,9 +18,40 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
-    id: "mapbox/streets-v11",
+    id: "mapbox/dark-v10",
     accessToken: API_KEY
   }).addTo(countyMap);
+
+  //load in data 
+
+  //var geoData = "static/data/complete.geojson"
+
+  //var json
+
+  //d3.json (geoData, function(data))
+
+  //geojson = L.choropleth(data, {
+    // Define what  property in the features to use
+    //valueProperty: 'MHI2016',
+    // Set color scale
+    //scale: ["#ffffb2", "#b10026"],
+
+    // Number of breaks in step range
+    //steps: 10,
+    // q for quantile, e for equidistant, k for k-means
+   // mode: "e",
+    // border styling
+    //style: {
+     // color: "#fff",
+      //weight: 1,
+      //fillOpacity: 0.8
+    //},
+    // Binding a pop-up to each layer
+    //onEachFeature: function(feature, layer) {
+      //layer.bindPopup("Zip Code: " + feature.properties.ZIP + "<br>Median Household Income<br>" + "$" + feature.properties.MHI2016)
+  //  }
+  //}).addTo(myMap);
+
 
 
 
