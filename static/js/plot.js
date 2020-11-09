4c29c44432e72524be7f46795fab2609d22e409d
Gina Ramirez <67246653+ramirezgc@users.noreply.github.com>
2020-11-09 15:11:49 -0600

    Made population donut chart work

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,6 +1,6 @@
 //Start donut chart plots
 let width = 450,
-        height = 450,
+        height = 500,
         margin = 40
 
 let radius = Math.min(width,height) / 2 - margin
@@ -12,18 +12,15 @@ let svg = d3.select("#myDiv")
   .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-d3.csv("../output/population.csv").then(function(countyData){
-  console.log(countyData);
+d3.csv("../output/population.csv").then(function(data){
+  console.log(data);
 
   let color = d3.scaleOrdinal()
-    .domain(countyData)
+    .domain(data)
     .range(d3.schemeDark2)
 
-  // Compute the position of)
-
-  // Compute the position of each group on the pie:
-  let pie = d3.pie().value(function(countyData){
-    return countyData.Population;
+  let pie = d3.pie().value(function(data){
+    return data.Population;
   });
 
   let path = d3.arc()
@@ -34,43 +31,26 @@ d3.csv("../output/population.csv").then(function(countyData){
       .innerRadius(radius-80)
       .outerRadius(radius)
 
-  // // Create dummy data
-  // d3.csv("../output/population.csv").then(function(countyData){
-  //   console.log(countyData);
-
   let arc = svg.selectAll(".arc")
-              .data(pie(countyData))
+              .data(pie(data))
               .enter()
-              .attr("class", "arc");
+             
 
-  arc.append("path")
-  .attr("d", path)
-  .attr("fill", function(d) { return color(d.data.County); });
+    arc.append("path")
+      .attr("d", path)
+      .attr("fill", function(d) { return color(d.data.County); });
 
-  console.log(arc)
+    console.log(arc)
 
-  arc.append("text")
-      .attr("transform", function(d) { 
-              return "translate(" + label.centroid(d) + ")"; 
-      })
-      .text(function(d) { return d.data.County; });
-  });
+    arc.append("text")
+        .attr("transform", function(d) { 
+                return "translate(" + label.centroid(d) + ")"; 
+        })
+        .text(function(d) { return d.data.County; });
+    });
 
-  svg.append("g")
-      .attr("transform", "translate(" + (width / 2 - 120) + "," + 20 + ")")
-      .append("text")
-      .text("Population")
-      .attr("class", "title")
-
-  // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-  // svg
-  //   .selectAll('allslices')
-  //   .data(data_ready)
-  //   .enter()
-  //   .attr('fill', function(d){ return(color(d.data.County)) })
-  //   .attr("stroke", "black")
-  //   .style("stroke-width", "2px")
-  //   .style("opacity", 0.7)
-  //   .text("Population")
-
-
+    svg.append("g")
+        .attr("transform", "translate(" + (width / 2 - 120) + "," + 20 + ")")
+        .append("text")
+        .text("Population")
+        .attr("class", "title")
