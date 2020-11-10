b55c593fed0a468648ca216c231ba12e0bc14ba4
TiaraSGreen <65461540+TiaraSGreen@users.noreply.github.com>
2020-11-09 18:14:35 -0600

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -48,7 +48,7 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     //},
     // Binding a pop-up to each layer
     //onEachFeature: function(feature, layer) {
-      //layer.bindPopup("County: " + feature.properties.County + "<br>Population<br>" + "$" + feature.properties.MHI2016)
+      //layer.bindPopup("County: " + feature.properties.County + "<br>Population<br>" + feature.properties.MHI2016)
   //  }
   //}).addTo(myMap);
 
