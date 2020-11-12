3a62832aeb0da0dd2e28fbe663c96c042f9d98e3
Gina Ramirez <67246653+ramirezgc@users.noreply.github.com>
2020-11-12 16:44:22 -0600

    Donut chart formatting complete

--- static/js/plot.js
+++ static/js/plot.js
@@ -17,12 +17,12 @@ function optionChanged(name) {
   console.log("County name=", name);
 
   county_info(name)
-  // county_population(name)
   county_poverty(name)
   county_unemployed(name)
   county_crime(name)
   
 }
+
 
 function county_info(countyName) {
   d3.csv("../output/complete.csv").then(function (data) {
@@ -34,6 +34,13 @@ function county_info(countyName) {
       }
     });
 
+
+    if (countyName == "") {
+      income = ""
+      crime = ""
+      population = ""
+    }
+
     let fig = d3.select("#county-metadata");
     fig.html("");
     
@@ -41,30 +48,26 @@ function county_info(countyName) {
     fig.append("h2").text(`Median household income: ${income}`);
     fig.append("h2").text(`Total crime events: ${crime}`);
     fig.append("h2").text(`County population: ${population}`);
+
+
+    
   });
 }
-
-// function county_population(countyName) {
-//   d3.csv("../output/complete.csv").then(function (data) {
-//     data.forEach(function (d) {
-//       if (d.County == countyName) {
-//       total_population = d.Population;
-//       }
-//     });
-  
-//     console.log("County_population: ", total_population);
-//   });
-// }
 
 function county_poverty(countyName) {
   d3.csv("../output/complete.csv").then(function (data) {
     data.forEach(function (d) {
       if (d.County == countyName) {
-        poverty_pop = d.Population * (d.Poverty_Percent*.01);
+        poverty_pop = d3.format(".0f")(d.Population * (d.Poverty_Percent*.01));
         not_poverty = d.Population - poverty_pop
       }
     });
   
+    if (countyName == "") {
+      poverty_pop = ""
+      not_poverty = ""
+    }
+
     console.log("County_poverty: ", poverty_pop);
 
      let poverty_data = {
@@ -75,19 +78,19 @@ function county_poverty(countyName) {
     let trace1 = [
       {
           values: [poverty_pop, not_poverty] ,   
-          text: ["In poverty"],
+          text: ["In poverty", "Not in poverty"],
+          textfont:{family: "Cabin Sketch", size: 10 },    
           marker: {
-              colors: ["lightblue", "navy"]
+            colors: ["lightblue", "navy"]
           },
           hole: .4,
           type: 'pie',
           rotation: 90,
           direction: "clockwise",
-          // textinfo: 'text',
-          // textposition: 'inside',
+          hoverinfo: "text+value",
           showlegend: false,
           domain: {
-              row:0,
+              row:1,
               column: 0
             },
       }
@@ -95,8 +98,9 @@ function county_poverty(countyName) {
   let layout = { width: 300,
       height: 300,
       margin: { t: 45, b: 10 },
-      title: "Poverty Rate",
-      font: { color: "royalblue", family: "Arial", size: 18 },    
+      title: "Poverty Rate", 
+      titlefont:{ color: "royalblue", family: "Cabin Sketch", size: 18 }, 
+      grid: {rows: 1, columns: 0}     
     }
 
     Plotly.newPlot('povertyDiv', trace1, layout);
@@ -110,22 +114,30 @@ function county_unemployed(countyName) {
   d3.csv("../output/complete.csv").then(function (data) {
     data.forEach(function (d) {
       if (d.County == countyName) {
-        unemployed_pop = d.Population * (d.Unemployment_Rate*.01);
+        unemployed_pop = d3.format(".0f")(d.Population * (d.Unemployment_Rate*.01));
         employed_pop = d.Population - unemployed_pop
       }
     });
   
+    if (countyName == "") {
+      unemployed_pop = ""
+      employed_pop = ""
+    }
+
     console.log("County_unemployed ", unemployed_pop);
 
-     let poverty_data = {
+     let unemployment_data = {
       "unemployed": unemployed_pop,
       "employed": employed_pop
+
     }
   
+ 
     let trace2 = [
       {
           values: [unemployed_pop, employed_pop] ,   
           text: ["Unemployed", "Employed"],
+          textfont:{family: "Cabin Sketch", size: 10 },   
           marker: {
               colors: ["lightblue", "navy"]
           },
@@ -133,12 +145,11 @@ function county_unemployed(countyName) {
           type: 'pie',
           rotation: 90,
           direction: "clockwise",
-          // textinfo: 'text',
-          // textposition: 'inside',
+          hoverinfo: "text+value",
           showlegend: false,
           domain: {
               row: 1,
-              column: 0
+              column: 2
             },
       }
   ];
@@ -146,7 +157,8 @@ function county_unemployed(countyName) {
       height: 300,
       margin: { t: 45, b: 10 },
       title: "Unemployment Rate",
-      font: { color: "royalblue", family: "Arial", size: 18 },       
+      titlefont:{ color: "royalblue", family: "Cabin Sketch", size: 18 },
+      grid: {rows: 1, columns: 1}      
     }
 
     Plotly.newPlot('unemployedDiv', trace2, layout);
@@ -160,11 +172,16 @@ function county_crime(countyName) {
   d3.csv("../output/complete.csv").then(function (data) {
     data.forEach(function (d) {
       if (d.County == countyName) {
-        violent_crime= (d.Violent_Crime / d.Total_Crime)*.01;
-        property_crime = (d.Property_Crime / d.Total_Crime)*.01;
+        violent_crime= d.Violent_Crime;
+        property_crime = d.Property_Crime; 
       }
     });
   
+    if (countyName == "") {
+      violent_crime = ""
+      property_crime = ""
+    }
+
     console.log("Vioulent_crime ", violent_crime);
     console.log("Property_crime ", property_crime);
 
@@ -177,6 +194,7 @@ function county_crime(countyName) {
       {
           values: [violent_crime, property_crime] ,   
           text: ["Violent", "Property"],
+          textfont:{family: "Cabin Sketch", size: 10 }, 
           marker: {
               colors: ["lightblue", "navy"]
           },
@@ -184,22 +202,23 @@ function county_crime(countyName) {
           type: 'pie',
           rotation: 90,
           direction: "clockwise",
-          // textinfo: 'text',
-          // textposition: 'inside',
+          hoverinfo: "text+value",
           showlegend: false,
           domain: {
-              row: 0,
-              column: 1
+              row: 1,
+              column: 3
             },
       }
   ];
-  let layout = { width: 600,
-      height: 600,
+  let layout = { width: 300,
+      height: 300,
       margin: { t: 45, b: 10 },
       title: "Total Crime",
-      font: { color: "royalblue", family: "Arial", size: 18 },       
+      titlefont:{ color: "royalblue", family: "Cabin Sketch", size: 18 }, 
+      grid: {rows: 1, columns: 3}       
     }
 
+ 
     Plotly.newPlot('crimeDiv', trace3, layout);
 
   });
